957b2e143c048ac8932626a461e66fc140827ec7
mahdi kabiri <mkabiri50@yahoo.com>
2019-11-07 13:21:19 +0330

    second commit

--- src/components/NavBar/NavBarItems/NavBarItems.jsx
+++ src/components/NavBar/NavBarItems/NavBarItems.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import NavigationItem from "./NavBarItem/NavBarItem";
 
-import NavigationItem from './NavBarItem/NavBarItem';
+class navigationItems extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="navbar-nav">
+          {this.props.isAuth ? (
+            <p>به آسان شهر خوش آمدید</p>
+          ) : (
+            <p> لطفا ابتدا ثبت نام</p>
+          )}
+        </ul>
+      </nav>
+    );
+  }
+}
 
-
-const navigationItemsL = () => (
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <ul className="navbar-nav">
-        <NavigationItem link='/hoses' >آگهی ها</NavigationItem>
-        <NavigationItem link='/login'>ثبت نام / ورود</NavigationItem>   
-    </ul>
-    </nav>
-
-
-);
-export default navigationItemsL;
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.isAuth
+  };
+};
+export default connect(mapStateToProps)(navigationItems);
